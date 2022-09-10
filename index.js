5d770fe8afb5d62ff95f096c2b8492e517e3f4cd
Abhra Pal <palabhra123@gmail.com>
2022-09-10 16:30:22 +0530

    server established

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("Hello World");
-console.log("Welcome");
-console.log("Learning node");
+const express = require('express');
+const app = express()
+const port =5050;
+app.get('/',(req, res)=>{
+    res.send("Hello")
+
+})
+app.listen(8000, ()=>{
+    console.log("Server is running");
+})
